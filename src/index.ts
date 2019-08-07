ce79c7e9f4f91feb0f83e285ea4729351bfe668a
Imed Adel <aadelimed@gmail.com>
2019-08-07 13:21:46 +0200

    add space

--- src/index.ts
+++ src/index.ts
@@ -218,6 +218,7 @@ export const eva = {
     body: 1 / 3,
     heading: 1 / 3,
   },
+  space: [0, 4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 128, 256, 512],
 };
 
 export default eva;
